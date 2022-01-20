fa15b6a30c1049d34c8c6ae8fa69e01727e78788
Eamonn Webster <eamonn.webster@gmail.com>
2022-01-20 09:40:47 +0000

    iOS DBRoulette updates

--- Examples/DBRoulette/iOS/CocoaPodsProject/DBRoulette/DBRoulette/PhotoViewController.m
+++ Examples/DBRoulette/iOS/CocoaPodsProject/DBRoulette/DBRoulette/PhotoViewController.m
@@ -8,6 +8,7 @@
 #import <ObjectiveDropboxOfficial/ObjectiveDropboxOfficial.h>
 
 #import "PhotoViewController.h"
+#import <CommonCrypto/CommonDigest.h>
 
 @interface PhotoViewController ()
 
@@ -121,60 +122,101 @@
   [[client.filesRoutes downloadData:imagePath]
       setResponseBlock:^(DBFILESFileMetadata *result, DBFILESDownloadError *routeError, DBRequestError *error, NSData *fileData) {
         if (result) {
-          UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageWithData:fileData]];
-          imageView.contentMode = UIViewContentModeScaleAspectFit;
-          imageView.frame = CGRectMake(100, 100, 300, 300);
-          [imageView setCenter:CGPointMake(self.view.bounds.size.width/2, self.view.bounds.size.height/2)];
-          [self.view addSubview:imageView];
-          _currentImageView = imageView;
-          [self setFinished];
+          [self validateDownloadData: fileData contentHash: result.contentHash];
+          [self displayImage: fileData];
         } else {
-          NSString *title = @"";
-          NSString *message = @"";
-          if (routeError) {
-            // Route-specific request error
-            title = @"Route-specific error";
-            if ([routeError isPath]) {
-              message = [NSString stringWithFormat:@"Invalid path: %@", routeError.path];
-            } else if ([routeError isOther]) {
-              message = [NSString stringWithFormat:@"Unknown error: %@", routeError];
-            }
-          } else {
-            // Generic request error
-            title = @"Generic request error";
-            if ([error isInternalServerError]) {
-              DBRequestInternalServerError *internalServerError = [error asInternalServerError];
-              message = [NSString stringWithFormat:@"%@", internalServerError];
-            } else if ([error isBadInputError]) {
-              DBRequestBadInputError *badInputError = [error asBadInputError];
-              message = [NSString stringWithFormat:@"%@", badInputError];
-            } else if ([error isAuthError]) {
-              DBRequestAuthError *authError = [error asAuthError];
-              message = [NSString stringWithFormat:@"%@", authError];
-            } else if ([error isRateLimitError]) {
-              DBRequestRateLimitError *rateLimitError = [error asRateLimitError];
-              message = [NSString stringWithFormat:@"%@", rateLimitError];
-            } else if ([error isHttpError]) {
-              DBRequestHttpError *genericHttpError = [error asHttpError];
-              message = [NSString stringWithFormat:@"%@", genericHttpError];
-            } else if ([error isClientError]) {
-              DBRequestClientError *genericLocalError = [error asClientError];
-              message = [NSString stringWithFormat:@"%@", genericLocalError];
-            }
-          }
-
-          UIAlertController *alertController =
-              [UIAlertController alertControllerWithTitle:title
-                                                  message:message
-                                           preferredStyle:(UIAlertControllerStyle)UIAlertControllerStyleAlert];
-          [alertController addAction:[UIAlertAction actionWithTitle:@"OK"
-                                                              style:(UIAlertActionStyle)UIAlertActionStyleCancel
-                                                            handler:nil]];
-          [self presentViewController:alertController animated:YES completion:nil];
-
-          [self setFinished];
+          [self downloadRouteError: routeError error: error];
         }
+        [self setFinished];
       }];
+}
+
+- (void)validateDownloadData:(NSData *)data contentHash: (NSString*)contentHash {
+    NSString * computedHash = [self computeContentHash: data];
+    NSLog(@"Dropbox  contentHash: '%@'", contentHash);
+    NSLog(@"Computed contentHash: '%@'", computedHash);
+}
+
+- (NSString *)computeContentHash:(NSData *)data {
+    long block_offset = 0;
+    NSMutableData *hashes = [NSMutableData data];
+    unsigned char digest[CC_SHA256_DIGEST_LENGTH] = {0};
+    while (block_offset < data.length) {
+        long block_length = data.length - block_offset;
+        if (block_length > 0x400000) {
+            block_length = 0x400000;
+        }
+        NSData* chunk = [NSData dataWithBytesNoCopy:(char *)[data bytes] + block_offset
+                                                length:block_length
+                                          freeWhenDone:NO];
+        CC_SHA256(chunk.bytes, (CC_LONG)chunk.length, digest);
+        [hashes appendBytes:digest length:CC_SHA256_DIGEST_LENGTH];
+        block_offset += block_length;
+    }
+    CC_SHA256(hashes.bytes, (CC_LONG)hashes.length, digest);
+
+    NSData *sha256Data = [NSData dataWithBytes:digest length:CC_SHA256_DIGEST_LENGTH];
+
+    NSMutableString *str = [[NSMutableString alloc] init];
+    const char *bytes = sha256Data.bytes;
+    for (int i = 0; i < [sha256Data length]; i++) {
+      [str appendFormat:@"%02hhx", bytes[i]];
+    }
+    return str;
+}
+
+- (void)displayImage:(NSData *)fileData {
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageWithData:fileData]];
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    imageView.frame = CGRectMake(100, 100, 300, 300);
+    [imageView setCenter:CGPointMake(self.view.bounds.size.width/2, self.view.bounds.size.height/2)];
+    [self.view addSubview:imageView];
+    _currentImageView = imageView;
+}
+
+- (void)downloadRouteError:(DBFILESDownloadError *)routeError error:(DBRequestError *)error {
+    NSString *title = @"";
+    NSString *message = @"";
+    if (routeError) {
+      // Route-specific request error
+      title = @"Route-specific error";
+      if ([routeError isPath]) {
+        message = [NSString stringWithFormat:@"Invalid path: %@", routeError.path];
+      } else if ([routeError isOther]) {
+        message = [NSString stringWithFormat:@"Unknown error: %@", routeError];
+      }
+    } else {
+      // Generic request error
+      title = @"Generic request error";
+      if ([error isInternalServerError]) {
+        DBRequestInternalServerError *internalServerError = [error asInternalServerError];
+        message = [NSString stringWithFormat:@"%@", internalServerError];
+      } else if ([error isBadInputError]) {
+        DBRequestBadInputError *badInputError = [error asBadInputError];
+        message = [NSString stringWithFormat:@"%@", badInputError];
+      } else if ([error isAuthError]) {
+        DBRequestAuthError *authError = [error asAuthError];
+        message = [NSString stringWithFormat:@"%@", authError];
+      } else if ([error isRateLimitError]) {
+        DBRequestRateLimitError *rateLimitError = [error asRateLimitError];
+        message = [NSString stringWithFormat:@"%@", rateLimitError];
+      } else if ([error isHttpError]) {
+        DBRequestHttpError *genericHttpError = [error asHttpError];
+        message = [NSString stringWithFormat:@"%@", genericHttpError];
+      } else if ([error isClientError]) {
+        DBRequestClientError *genericLocalError = [error asClientError];
+        message = [NSString stringWithFormat:@"%@", genericLocalError];
+      }
+    }
+
+    UIAlertController *alertController =
+        [UIAlertController alertControllerWithTitle:title
+                                            message:message
+                                     preferredStyle:(UIAlertControllerStyle)UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK"
+                                                        style:(UIAlertActionStyle)UIAlertActionStyleCancel
+                                                      handler:nil]];
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 - (void)setStarted {
