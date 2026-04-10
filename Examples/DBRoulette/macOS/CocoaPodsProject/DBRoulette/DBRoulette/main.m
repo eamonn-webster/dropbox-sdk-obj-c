81ab85dad17b092f08a5c95f79afabc432081b34
Eamonn Webster <eamonn.webster@gmail.com>
2026-04-10 08:46:32 +0100

    symlink rather than duplicate

--- Examples/DBRoulette/macOS/CocoaPodsProject/DBRoulette/DBRoulette/main.m
+++ Examples/DBRoulette/macOS/CocoaPodsProject/DBRoulette/DBRoulette/main.m
@@ -1,13 +1,1 @@
-//
-//  main.m
-//  DBRoulette
-//
-//  Created by Stephen Cobbe on 2/27/17.
-//  Copyright © 2017 Dropbox. All rights reserved.
-//
-
-#import <Cocoa/Cocoa.h>
-
-int main(int argc, const char * argv[]) {
-  return NSApplicationMain(argc, argv);
-}
+../../../CarthageProject/DBRoulette/DBRoulette/main.m
